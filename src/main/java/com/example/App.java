b702f851831dc602e09da0382b7673f98acfe6f0
RODHERMA81 <arjo1981@gmail.com>
2024-05-31 14:45:02 +0200

    Con pattern matching

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,55 +4,43 @@ package com.example;
  * Hello world!
  *
  */
-public class App 
-{
-	
-    public static void main( String[] args )
-    {
-  
-    	// Queremos crear un array que contenga tanto personas como coches en el mismo.
-    	
-    	Object[] personasYCoches = {
-    		
-    		Persona.builder()
-    		.nombre("Rosa")
-    		.primerApellido("Castro")
-    		.genero(Genero.MUJER)
-    		.build(),
-    		Persona.builder()
-    		.nombre("Marly")
-    		.genero(Genero.MUJER)
-    		.build(),
-    		Coche.builder()
-    		.marca("Audi")
-    		.modelo("R2")
-    		.build(),
-    		Coche.builder()
-    		.marca("Mercedes")
-    		.modelo("AMG")
-    		.build()
-    		
-    			
-    	};
-    	
-    	// Recorrer el array y operar con los elementos segun el tipo 
-    	
-    	for(Object object : personasYCoches) {
-    		
-    		// Tengo que determinar si el objeto es una instancia de Persona o de Coche
-    		// para lo cual se utiliza el operador instanceof, que comprueba si un objeto
-    		// es una instancia de algun tipo.
-    		
-    	if(object instanceof Persona) {
-    		Persona p = (Persona) object;
-    		System.out.println("Nombre de la persona: " + p.getNombre());
-    	
-    	} else if(object instanceof Coche) {
-    		Coche c = (Coche) object;
-    		System.out.println("La marca del coche es: " + c.getMarca());
-    		System.out.println("El pais del fabricante es: " + Coche.paisDelFabricante);
-    	
-    	}
-    	
-    }
-    }
+public class App {
+
+	public static void main(String[] args) {
+
+		// Queremos crear un array que contenga tanto personas como coches en el mismo.
+
+		Object[] personasYCoches = {
+
+				Persona.builder().nombre("Rosa").primerApellido("Castro").genero(Genero.MUJER).build(),
+				Persona.builder().nombre("Marly").genero(Genero.MUJER).build(),
+				Coche.builder().marca("Audi").modelo("R2").build(),
+				Coche.builder().marca("Mercedes").modelo("AMG").build()
+
+		};
+
+		// Recorrer el array y operar con los elementos segun el tipo
+
+		for (Object object : personasYCoches) {
+
+			// Tengo que determinar si el objeto es una instancia de Persona o de Coche
+			// para lo cual se utiliza el operador instanceof, que comprueba si un objeto
+			// es una instancia de algun tipo.
+
+			// CON PATTERN MATCHING
+
+			if (object instanceof Persona p) {
+				// Persona p = (Persona) object;
+				System.out.println("Nombre de la persona: " + p.getNombre());
+
+			} else if (object instanceof Coche c) {
+				// Coche c = (Coche) object;
+				System.out.println("La marca del coche es: " + c.getMarca());
+				System.out.println("El pais del fabricante es: " + Coche.paisDelFabricante);
+
+			}
+
+		}
+	}
+
+}
